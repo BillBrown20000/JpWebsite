dab9ace0d76f8b0d3410b52824f8a3b706cd65b6
Evan Haggard <evanhaggard123@gmail.com>
2020-05-15 20:34:54 -0400

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { NavbarTempleteComponent } from './navbar-templete/navbar-templete.component';
-=======
 import { LogoComponent } from './logo/logo.component';
 import { LogoTemplateComponent } from './logo-template/logo-template.component';
->>>>>>> 72c1e3db4cd482e799804f7e662cea7597588204
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    NavbarTempleteComponent
-=======
+    NavbarTempleteComponent,
     LogoComponent,
     LogoTemplateComponent
->>>>>>> 72c1e3db4cd482e799804f7e662cea7597588204
   ],
   imports: [
     BrowserModule,
