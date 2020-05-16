3321b4cd2233321dcf3856def1d4524fb3c4c38e
alexiswill <yukiotakao7@gmail.com>
2020-05-16 15:20:10 -0400

    added hero banner

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BannerComponent } from './banner/banner.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BannerComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
